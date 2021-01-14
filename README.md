6d5e34bcf5bd33b9def8d6189cef9249c772c742
Dechev Daniel Ivanov <datas2k@gmail.com>
2021-01-14 23:34:43 +0100

    1.0

---
+++ README.md
@@ -0,0 +1,7 @@
+# [theAteam](https://smokey2k.github.io/theAteam/)
+
+tHeSupErsTitious Alpha male team collaboration in project Türr
+
+![alt text](images/readme_banner.png)
+
+Check our site @ [GitHub Pages](https://smokey2k.github.io/theAteam/).
---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+console.log("Hello, World!");
+
+$(document).ready(function(){
+    $("#page1").click(function(){
+        $("#grid-area-1222").load("pages/page1.html");
+    });
+    $("#page2").click(function(){
+        $("#grid-area-1222").load("pages/page2.html");
+    });
+    $("#page3").click(function(){
+        $("#grid-area-1222").load("pages/page3.html");
+    });
+    $("#page4").click(function(){
+        $("#grid-area-1222").load("pages/page4.html");
+    });
+    $("#page5").click(function(){
+        $("#grid-area-1222").load("pages/page5.html");
+    });
+    $("#page6").click(function(){
+        $("#grid-area-1222").load("pages/page6.html");
+    });
+    $("#page7").click(function(){
+        $("#grid-area-1222").load("pages/page7.html");
+    });
+});
