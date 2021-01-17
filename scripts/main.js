5f405dbc5371672b39fb18a7ec4dfe6863670394
smokey2k <datas2k@gmail.com>
2021-01-17 15:27:38 +0100

    lulababe

--- scripts/main.js
+++ scripts/main.js
@@ -5,27 +5,17 @@
     a legtöbb böngészőben.A jQuery támogatja ezt az eljárást a holdReady() funkcióval.
     További információ: https://api.jquery.com/jquery.holdready/
 */
-function loadPageScript(pageurl) {
-    $.holdReady(true);
-    function releaseHold() { $.holdReady(false); }
-    $.getScript(pageurl, releaseHold);
-}
+    function loadPageScript(pageurl) {
+        $.holdReady(true);
+        function releaseHold() { $.holdReady(false); }
+        $.getScript(pageurl, releaseHold);
+    }
 
 $(document).ready(function(){
-    let oldalak = [
-        ['.logo', "page0"],
-        ['#page1', "page1"],
-        ['#page2', "page2"],
-        ['#page3', "page3"],
-        ['#page4', "page4"],
-        ['#page5', "page5"],
-        ['#page6', "page6"],
-        ['#page7', "page7"]
-    ];
-    for (let i = 0; i < 7; i++) {
-        $(oldalak[i][0]).click(function(){
-            $("#grid-area-1222").load(`pages/${oldalak[i][1]}.html`);
-            loadPageScript(`/scripts/${oldalak[i][1]}.js`);
+    for (let i = 0; i < 8; i++) {
+        $(`#page${i}`).click(function(){
+            $("#grid-area-1222").load(`pages/page${i}.html`);
+            loadPageScript(`/scripts/page${i}.js`);
         });
     }
 });
