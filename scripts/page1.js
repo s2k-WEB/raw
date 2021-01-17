8808808f6d255e96626bd2242438ebac1ff1c5d2
smokey2k <datas2k@gmail.com>
2021-01-17 15:30:05 +0100

    rhcp

--- scripts/page1.js
+++ scripts/page1.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    var nyeroszamok = [30, 57, 58, 61, 86, 30, 57, 58, 61, 86];
+    const nyeroszamok = [30, 57, 58, 61, 86, 30, 57, 58, 61, 86];
     for (let i = 0; i < nyeroszamok.length; i++) {
         document.getElementById("golyo" + i).innerHTML = nyeroszamok[i];        
     }
